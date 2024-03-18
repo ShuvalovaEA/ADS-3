4a1bb0a6e664c35fa9eddf2d0716a42e290461ce
ShuvalovaEA <eshalos1207@gmail.com>
2024-03-18 17:04:48 +0300

    com

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,16 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int low, count = 0, high = size - 1;
+  while (low < high){
+      int middle = (low + high) / 2;
+      if (arr[middle] < value) {
+          low = middle + 1;
+      } else if (arr[middle] >= value) {
+          high = middle - 1;
+      } else {
+          return middle;
+      }
+  }
+  return 0;
 }
